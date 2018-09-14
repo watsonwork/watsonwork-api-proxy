99bebdfbaa00de864c86b46582c217091361127f
Olgierd Pieczul <olgierdp@ie.ibm.com>
2018-09-14 12:04:09 +0100

    initial comit

---
+++ test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const id = process.env.TEST_CLIENT_ID;
+const secret = process.env.TEST_CLIENT_SECRET;
+
+describe('testing proxy', () => {
+  let server;
+
+  before(function () {
+    server = require('./app');
+  });
+
+  after(function () {
+    server.close();
+  });
+
+  it('graphQL me() ', (done) => {
+    for (let i = 0; i < 2; i++) { // run twice to test caching
+      request(server)
+        .post('/graphql')
+        .auth(id, secret)
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/graphql')
+        .send('query { me { id } }')
+        .expect(200, { data: { me: { id: id } } }, done);
+    }
+  });
+});
